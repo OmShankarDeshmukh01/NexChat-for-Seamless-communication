acddb02ca65285034ad4eed372e0ac2209b2045c
OmShankarDeshmukh01 <omdeshmukhstudy@gmail.com>
2024-07-30 20:25:48 +0530

    added changes

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,7 +1,17 @@
 import { useAppStore } from "@/store";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Profile =()=>{
-    const {userInfo} = useAppStore()
+    const navigate = useNavigate();
+    const {userInfo , setUserInfo} = useAppStore();
+
+    const [firstName , setFirstName] = useState("");
+    const [lastName , setLastName] = useState("");
+    const [image , setImage] = useState(null);
+    const [hovered , setHovered] = useState(false);
+    const [selectedColor , setSelectedColor] = useState(0);
+
     return(
         <div>
             Profile
