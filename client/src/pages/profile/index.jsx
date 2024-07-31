7f64ef68bc8e4d2f9046eddde2a1a370fd45413a
OmShankarDeshmukh01 <omdeshmukhstudy@gmail.com>
2024-07-31 20:48:29 +0530

    added files

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,6 +1,8 @@
 import { useAppStore } from "@/store";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 const Profile =()=>{
     const navigate = useNavigate();
@@ -12,10 +14,27 @@ const Profile =()=>{
     const [hovered , setHovered] = useState(false);
     const [selectedColor , setSelectedColor] = useState(0);
 
+    const saveChanges = async ()=>{
+
+    }
+
     return(
-        <div>
-            Profile
-            <div>Email:{userInfo.id}</div>
+        <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
+            <div className="flex flex-col gap-10 w-[80vw] md:w-max">
+                <div>
+                    <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer"/>
+                </div>
+                <div className="grid grid-cols-2">
+                    <div className="h-full w-32 md:w-48 md:h-48 relative flex items-center justify-center" onMouseEnter={()=> setHovered(true)} onMouseLeave={()=> setHovered(false)}>
+                        <Avatar className="h-32 w-32 md:w-48 md:h-48 rounded-full overflow-hidden">
+                            { image ? (<AvatarImage src={image} alt="profile" className="object-cover w-full h-full bg-black"/> ): (<div className="uppercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[1px] flex items-center justify-center rounded-full">
+                                {firstName ? firstName.split("").shift() : userInfo.email.split("").shift()}
+                                </div>
+                            )}
+                        </Avatar>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
